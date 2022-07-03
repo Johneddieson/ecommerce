84067c5b4a213490b18d5a8dd3ab4bfb0c07fec6
unknown <vergaraeddieson@gmail.com>
2022-07-04 00:54:04 +0800

    Increase, Decrease Quantity function in shopping cart

--- ecommerce/src/app/home/home.page.ts
+++ ecommerce/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AlertController } from '@ionic/angular';
 import { AuthServiceService } from '../auth-service.service';
 
 @Component({
@@ -9,19 +10,37 @@ import { AuthServiceService } from '../auth-service.service';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor(private auth: AuthServiceService,  private afstore: AngularFirestore, private afauth: AngularFireAuth) {
+numbers = 0;
+showLog = false
+  constructor(private alertCtrl: AlertController, private auth: AuthServiceService,  private afstore: AngularFirestore, private afauth: AngularFireAuth) {
 
     this.afauth.authState.subscribe(user => {
 
       if (user && user.uid) {
-
-      
       }
     })
    }
 
   ngOnInit() {
+    this.numbers = 1
   }
 
+  Increase() {
+    return this.numbers++;
+    
+  }
+  Decrease() {
+    if (this.numbers == 1) {
+this.alertCtrl.create({
+  message: "The Quantity should'nt be zero",
+  buttons: ["Ok"]
+}).then(el => {
+  el.present()
+})
+return this.numbers = 1;
+    } else {
+      return this.numbers--;
+
+    }
+  }
 }
