97bb26d87b1a4a69e022edd2567e34bb39637389
unknown <vergaraeddieson@gmail.com>
2022-07-05 00:07:46 +0800

    Add to Cart Function, and create new product

--- ecommerce/src/app/add-product/add-product.page.ts
+++ ecommerce/src/app/add-product/add-product.page.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { AttrAst } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { loadingController } from '@ionic/core';
 
 @Component({
   selector: 'app-add-product',
@@ -8,11 +12,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.page.scss'],
 })
 export class AddProductPage implements OnInit {
-photoLink: any
-  constructor(public http: HttpClient) { }
+  registerForm: FormGroup;
+  photoLink: any
+  withPhoto: boolean = false
+  constructor(public http: HttpClient, public formBuilder: FormBuilder, public loadingCtrl: LoadingController, public alertCtrl: AlertController,
+    private afstore: AngularFirestore) {
+   
+   }
 
   ngOnInit() {
+    this.photoLink = 'https://static.wikia.nocookie.net/otonari-no-tenshi/images/c/c9/No_images_available.jpg/revision/latest?cb=20220104141308'
+    this.registerForm = new FormGroup({
+      firstname: new FormControl('', [
+        Validators.required,
+        this.customPatternValid({ pattern: /^([A-Z][a-z]*((\s[A-Za-z])?[a-z]*)*)$/, msg: "Always Starts With Capital Letter"}),
+        this.customPatternValid({ pattern: /^([^0-9]*)$/, msg: 'Numbers is not allowed' }),
+        Validators.minLength(5),
+        // Validators.maxLength(10),
+      ]),
+      // middlename: new FormControl('', [
+      //   Validators.required,
+      //   this.customPatternValid({ pattern: /^([A-Z][a-z]*((\s[A-Za-z])?[a-z]*)*)$/, msg: "Always Starts With Capital Letter"}),
+      //   this.customPatternValid({ pattern: /^([^0-9]*)$/, msg: 'Numbers is not allowed' }),
+      //   Validators.minLength(5),
+      //   Validators.maxLength(10)
+      // ]),
+      // surname: new FormControl('', [
+      //   Validators.required,
+      //   this.customPatternValid({ pattern: /^([A-Z][a-z]*((\s[A-Za-z])?[a-z]*)*)$/, msg: "Always Starts With Capital Letter"}),
+      //   this.customPatternValid({ pattern: /^([^0-9]*)$/, msg: 'Numbers is not allowed' }),
+      //   Validators.minLength(5),
+      // ]),
+      cellphonenumber: new FormControl('', [
+        Validators.required,
+        // Validators.pattern("^[0&9]{2}[0-9]{9}")
+             this.customPatternValid({ pattern: /^[+-]?(?:\d*[1-9]\d*(?:\.\d+)?|0+\.\d*[1-9]\d*)$/, msg: 'This format is not allowed' }),
+             this.customPatternValid({ pattern: /^([^.?!-]*)$/, msg: 'Negative is not allowed' }),
+      
+    ]),
+      password: new FormControl('', [
+  Validators.required,
+  this.customPatternValid({ pattern: /^[+-]?(?:\d*[1-9]\d*(?:\.\d+)?|0+\.\d*[1-9]\d*)$/, msg: 'This format is not allowed' }),
+  this.customPatternValid({ pattern: /^([^-]*)$/, msg: 'Negative is not allowed' }),
+
+])
+    })
   }
+  customPatternValid(config: any): ValidatorFn {
+    console.log("wew", config)
+    return (control: FormControl) => {
+      let urlRegeX: RegExp = config.pattern;
+      if (control.value && !control.value.match(urlRegeX)) {
+        return {
+          invalidMsg: config.msg
+        };
+      } else {
+        return null
+      }
+    }
+      }
   fileChanged(event) {
     const files = event.target.files
     console.log("the files", files)
@@ -28,7 +86,63 @@ photoLink: any
 
         }
       }
+    this.withPhoto = true
     })
+  }
+  submit() {
+    if (this.withPhoto == false) {
+        this.alertCtrl.create({
+          message: 'Please Choose a Photo',
+          buttons: [
+          {
+            text: 'Ok',
+            role: 'cancel'
+          }
+          ]
+        }).then(els2 => {
+          els2.present()
+        })
+    } else {
+
+    
+    this.loadingCtrl.create({
+      message: 'Creating New Product'
+    }).then(el => {
+      el.present()
+
+      
+      this.afstore.collection('Products').add({
+        ProductName: this.registerForm.value.firstname,
+        Stock: this.registerForm.value.cellphonenumber,
+        UnitPrice: this.registerForm.value.password,
+        ImageUrl: this.photoLink,
+        Quantity: 1
+      })
+
+      setTimeout(() => {
+        el.dismiss()   
+        this.registerForm.reset()
+        this.photoLink = 'https://static.wikia.nocookie.net/otonari-no-tenshi/images/c/c9/No_images_available.jpg/revision/latest?cb=20220104141308'
+        this.alertCtrl.create({
+          header: 'Officially Created',
+          message: 'You created the product successfully',
+          buttons: [
+            {
+              text: 'Ok',
+              role: 'cancel'
+            }
+          ]
+        }).then(els => {
+          els.present()
+        }).catch(err => {
+
+        })
+      }, 3000)
+       
+    }).catch(err => {
+
+    })
+  }
   }
 
 }
--- ecommerce/src/app/home/home.page.ts
+++ ecommerce/src/app/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { AlertController } from '@ionic/angular';
 import { AuthServiceService } from '../auth-service.service';
-
+import { map } from 'rxjs/operators';
+import * as firebase from 'firebase/app'
+import { MessengerService } from '../messenger.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -12,35 +14,101 @@ import { AuthServiceService } from '../auth-service.service';
 export class HomePage implements OnInit {
 numbers = 0;
 showLog = false
-  constructor(private alertCtrl: AlertController, private auth: AuthServiceService,  private afstore: AngularFirestore, private afauth: AngularFireAuth) {
+productReference: AngularFirestoreCollection
+sub
+productList: any[] = []
+getCartDetails: any = []
+cartItem:number = 0
+  constructor(private msg: MessengerService, private alertCtrl: AlertController, private auth: AuthServiceService,  private afstore: AngularFirestore, private afauth: AngularFireAuth) {
 
     this.afauth.authState.subscribe(user => {
 
       if (user && user.uid) {
+       this.productReference =  afstore.collection('Products')
+       this.sub = this.productReference.snapshotChanges().pipe(map(actions => actions.map(a => {
+        return {
+          id: a.payload.doc.id,
+          ...a.payload.doc.data() as any
+        }
+       }))).subscribe(data => {
+        this.productList = data
+       })   
       }
     })
    }
 
   ngOnInit() {
-    this.numbers = 1
+this.loadCart()
   }
-
-  Increase() {
-    return this.numbers++;
-    
+loadCart() {
+  if (localStorage.getItem('cart') != null) {
+var thearray = []
+    thearray.push(JSON.parse(localStorage.getItem('cart')))
+    this.numbers = thearray.length;
+  } 
+}
+  Increase(data) {
+    data.Quantity +=1
+  this.loadCart()
   }
-  Decrease() {
-    if (this.numbers == 1) {
-this.alertCtrl.create({
-  message: "The Quantity should'nt be zero",
-  buttons: ["Ok"]
-}).then(el => {
-  el.present()
-})
-return this.numbers = 1;
+  Decrease(data) {
+    if (data.Quantity == 1) {
+      this.alertCtrl.create({
+        message: 'Quantity should not be zero',
+        buttons: [
+          {
+            text: 'Ok',
+            role: 'cancel'
+          }
+        ]
+      }).then(el => {
+        el.present()
+        this.loadCart()
+      })
     } else {
-      return this.numbers--;
-
+      data.Quantity -= 1
+this.loadCart()
     }
+  }
+  alertNgam() {
+  }
+  itemsCart: any = []
+  AddtoCart(data) {
+    var cartData = localStorage.getItem('cart')
+    if (cartData == null) {
+      let storageDataGet: any = []
+      storageDataGet.push(data)
+      localStorage.setItem('cart', JSON.stringify(storageDataGet))
+
+    } else {
+      var id = data.id
+      let index: number = -1
+
+      this.itemsCart = JSON.parse(localStorage.getItem('cart'))
+      for (let i= 0; i<this.itemsCart.length; i++) {
+        if (id == this.itemsCart[i].id) {
+          this.itemsCart[i].Quantity = data.Quantity
+          index = i;
+          break;
+        }
+      }
+    
+      if (index == -1) {
+          this.itemsCart.push(data)
+          
+        localStorage.setItem('cart', JSON.stringify(this.itemsCart))
+      } else {
+        localStorage.setItem('cart', JSON.stringify(this.itemsCart))
+      }
+    this.cartItemFunc()
+   
+    }
+    this.loadCart()
+  }
+  cartItemFunc() {
+    var cartValue = JSON.parse(localStorage.getItem('cart')) 
+      this.cartItem = cartValue.length
+    this.msg.cartSubject.next(this.cartItem)
+  
   }
 }
