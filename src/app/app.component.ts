1fa88d31090729ec1cc268b1b9b86e71c1b811e5
unknown <vergaraeddieson@gmail.com>
2023-07-24 03:55:42 +0800

    For Final Defense

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging/sw";
 import { environment } from 'src/environments/environment';
+import { DbserviceService } from './services/dbservice.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor() 
+  constructor(private dbservice: DbserviceService) 
   {
-    
-    
+    // this.dbservice.getData('Products')
+    // .subscribe((data) => 
+    // {
+    //   data.forEach(fe => 
+    //     {
+    //       var specific = 
+    //       {
+    //         Materials: []
+    //       }
+    //       this.dbservice.updateData(fe.id, specific, 'Products').then(() => 
+    //       {
+    //         console.log("materials updated")
+    //       })
+    //     })
+    // })
   }
 
 ngOnInit(): void {
